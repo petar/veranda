53fa34aca99b4bf937c1cef077562e22297b9847
Petar Maymounkov <petar@tumblr.com>
2012-10-21 14:00:31 -0400

    ported a devweb

---
+++ blog/main.go
@@ -0,0 +1,14 @@
+// Copyright 2011 The Go Authors.  All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"code.google.com/p/rsc/devweb/slave"
+	_ "github.com/petar/p/blog/post"
+)
+
+func main() {
+	slave.Main()
+}
