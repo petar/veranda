fdb317f5e055f0c06bbcc8acd74ff335333e752a
Petar Maymounkov <petarm@gmail.com>
2013-08-02 12:28:52 -0400

    ok

---
+++ kc/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"code.google.com/p/rsc/keychain"
+)
+
+var (
+	flagServer = flag.String("s", "", "Server to look for in the keychain")
+	flagUser   = flag.String("u", "", "Preferred user to look for in the keychain or empty")
+)
+
+func main() {
+	flag.Parse()
+	usr, pwd, err := keychain.UserPasswd(*flagServer, *flagUser)
+	if err != nil {
+		println("Bad luck:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Printf("Server=%s User=%s Password=%s\n", *flagServer, usr, pwd)
+}
